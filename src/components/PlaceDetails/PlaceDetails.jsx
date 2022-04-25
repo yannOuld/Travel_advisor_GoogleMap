8104da3a1d8c010c633bbe0152519b08a1bfa292
Yannis ould abderrahmane <yannis.ouldab@outlook.fr>
2022-04-25 06:20:12 +0200

    restaurants card done

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -9,7 +9,7 @@ import {
     Button,
     Chip
 } from "@material-ui/core";
-import LocationOn from '@material-ui/icons/LocationOn';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import Phone from '@material-ui/icons/Phone';
 import { Rating } from "@material-ui/lab";
 
@@ -37,6 +37,33 @@ const PlaceDetails = ({ place }) => {
                     <Typography variant="subtitle1">Ranking</Typography>
                     <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
                 </Box>
+                {place?.awards?.map((award) => (
+                    <Box my={1} display="flex" justifyContent="space-between">
+                        <img src={award.images.small} alt={award.display_name} />
+                        <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+                    </Box>
+                ))}
+                {place?.cuisine?.map(({ name }) => (
+                    <Chip key={name} size="small" label={name} className={classes.chip} />
+                ))}
+                {place?.address && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.subtitle}>
+                        <LocationOnIcon /> {place.address}
+                    </Typography>
+                )}
+                {place?.phone && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.spacing}>
+                        <Phone /> {place.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+                        website
+                    </Button>
+                </CardActions>
             </CardContent>
         </Card>
     )
