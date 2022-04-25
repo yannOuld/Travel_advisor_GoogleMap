635e2d65cc8baf40e14fb13bf88bef42da43b29f
Yannis ould abderrahmane <yannis.ouldab@outlook.fr>
2022-04-25 03:28:04 +0200

    cards on progress

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import {
+    Box,
+    Typography,
+    Card,
+    CardMedia,
+    CardContent,
+    CardActions,
+    Button,
+    Chip
+} from "@material-ui/core";
+import LocationOn from '@material-ui/icons/LocationOn';
+import Phone from '@material-ui/icons/Phone';
+import { Rating } from "@material-ui/lab";
+
+import useStyles from './style'
 
 const PlaceDetails = ({ place }) => {
+    const classes = useStyles();
+
     return (
-        <h1>{place.name}</h1>
+        <Card elevation={6}>
+            <CardMedia
+                style={{ height: 350 }}
+                image={place.photo ? place.photo.images.large.url : 'https://images.pexels.com/photos/6275820/pexels-photo-6275820.jpeg'}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5">
+                    {place.name}
+                </Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                </Box>
+            </CardContent>
+        </Card>
     )
 };
 export default PlaceDetails;
